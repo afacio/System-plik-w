4088aaaea511b58975447a30390ed212fc8461c6
afacio <afacio@users.noreply.github.com>
2020-10-29 10:22:49 +0100

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import Interface
+
+def main():
+    interface = Interface.Interface()
+    interface.choice()
+
+main()
+
+if __name__ == "__main__":
+    main()
+
+
+#można dodać funkcje zapisywania po zakończonej edycji w pliku
